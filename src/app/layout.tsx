eeed39a23aab2235a235f4db90b7d5770750103d
fwas <fwas.corp@gmail.com>
2024-08-05 20:58:11 +0200

    clerk added, started to integrate it with supabase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,12 +18,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+import { auth } from "@clerk/nextjs/server";
 import PhotoHandlingWrapper from "./components/PhotoHandlingWrapper";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in")
+  }
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24">
       <PhotoHandlingWrapper />
